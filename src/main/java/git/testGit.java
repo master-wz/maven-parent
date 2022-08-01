c928949aa964bd573a009a2f0283c2aa4d8b9935
master-wz <110366985+master-wz@users.noreply.github.com>
2022-08-01 14:26:27 +0800

    Update testGit.java

--- src/main/java/git/testGit.java
+++ src/main/java/git/testGit.java
@@ -4,5 +4,6 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello1");
+        System.out.println("hello2");
     }
 }
